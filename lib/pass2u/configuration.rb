0365d7181e8b6f9fc39a272d4093ed6bf6576f02
Alexis Dumortier <alexis.dumortier@free.fr>
2024-05-14 10:02:33 +0200

    Add configuration

---
+++ lib/pass2u/configuration.rb
@@ -0,0 +1,17 @@
+module Pass2U
+  class Configuration
+
+    attr_accessor :api_key, :base_uri
+
+    def initialize
+      set_defaults
+    end
+
+    private
+
+    def set_defaults
+      @api_key = ''
+      @base_uri = 'https://api.pass2u.net/v2'
+    end
+  end
+end
