b566ea337db32dc566e37ad0fc25607aa4d0e44b
Alexis Dumortier <alexis.dumortier@free.fr>
2024-05-14 19:29:19 +0200

    Add custom errors

---
+++ lib/pass2u/error.rb
@@ -0,0 +1,21 @@
+module Pass2U
+  class Error < StandardError; end
+
+  class ApiResponseError < Error
+    attr_reader :original_error
+
+    def initialize(message, original_error)
+      super(message)
+      @original_error = original_error
+    end
+  end
+
+  class ApiConnectionError < Error
+    attr_reader :original_error
+
+    def initialize(message, original_error)
+      super(message)
+      @original_errror = original_error
+    end
+  end
+end
